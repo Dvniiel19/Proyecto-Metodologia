c55bc317166c21e5a624ead4413974f2e14f0194
dvniiel <danieell.parra@gmail.com>
2026-07-02 01:06:17 -0400

    Delete src directory

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-// Configuración de variables de entorno
-require('dotenv').config();
-
-const config = {
-  PORT: process.env.PORT || 8001,
-  NODE_ENV: process.env.NODE_ENV || 'development'
-};
-
-module.exports = config;
